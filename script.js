36942e6cfe9a05be7fd140bd174800960cdd052a
Rabab <76789581+rssulaiman@users.noreply.github.com>
2022-12-27 02:57:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,18 @@ function next(){
     return setGif();
 } */
 
+function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drag(event) { 
+    event.dataTransfer.setData("text", event.target.id);
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+     event.target.appendChild(document.getElementById(data));
+  }
+  
+  
